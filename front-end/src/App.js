7c0ec426ba93829cb81a7ac2d5d125508235dcb2
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-27 12:00:05 -0800

    API call in place and working.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,21 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=5')
+      .then (res => {
+        console.log('res: ', res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+  }, [])
+
   return (
     <div className="App">
      <h1>beastlesmBook</h1> 
