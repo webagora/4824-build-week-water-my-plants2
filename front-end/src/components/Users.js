6bc5c8aeb68b484045d5a0bb51577137283c3ac1
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-28 15:48:47 -0800

    Users information showing on users page

--- front-end/src/components/Users.js
+++ front-end/src/components/Users.js
@@ -4,11 +4,15 @@ const Users = (props) => {
 
     return (
         <div className = "user-wrapper">
-            {props.users.map(user=> (
-                <div> 
-                    {user.name}
+            {props.users.map(user=>( 
+                <div key = {user.uuid}> 
+                    <h2>{user.name}</h2>
+                    <img src = {user.picture} alt = {user.name} />
+                    <p>Contact: {user.email} </p>
+                    <p>Location: {user.location} </p>
+
                 </div>
-            )) }
+            ))}
         </div>   
     )
 }
