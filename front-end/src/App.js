a7b72db0b5c4cf1f4826c47dd7c11c7f8b766a5c
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-28 14:59:48 -0800

    Routing in place

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
+import { Route, Link } from 'react-router-dom'
+
+import Users from './components/Users'
+
+const admin = {
+  location: "NY",
+  uuid: "12345",
+  picture: "https://wallsdesk.com/wp-content/uploads/2016/12/Thor-High-Quality-Wallpapers.jpg",
+  name: "CRHarding",
+  email: "C@c.com"
+}
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -18,11 +29,28 @@ function App() {
 
   return (
     <div className="App">
-     <h1>beastlesmBook</h1> 
-     <h2>It's like facebook, only made by some guy called Beatlesm</h2>
-     <p>Coming soon!</p>
+     <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/users">Friends Page</Link>
+        </nav>
+      </header>
+      <Route path="/users">
+        <Users users={users} />
+      </Route>
+      
     </div>
   );
 }
 
 export default App;
+
+
+/* 
+* user.location -> city
+* user.uuid
+* user.picture.medium
+* user.name -> user.first + user.last
+* user.post -> stretch - likes
+* user.email
+*/
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router> 
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +19,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+ * match -> Grab any data from the url ie /users/:id -> we can get the id out
+ * location -> Where are you currently
+ * history -> Where have you been?
+ */
+
